d1e568d4685635a9afa61f684e663b501d147abf
wgz <490480770@qq.com>
2015-11-17 16:15:30 +0800

    1

--- app/src/main/java/com/wgz/ant/antinstall/LoginActivity.java
+++ app/src/main/java/com/wgz/ant/antinstall/LoginActivity.java
@@ -2,13 +2,21 @@ package com.wgz.ant.antinstall;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.Window;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.LinearLayout;
+import android.widget.Toast;
+
+import com.wgz.ant.antinstall.util.CheckLOGIN;
+import com.wgz.ant.antinstall.util.OnDataFinishedListener;
 
 /**
  * Created by qwerr on 2015/11/17.
@@ -28,15 +36,38 @@ public class LoginActivity extends Activity {
 
     private void initview() {
         login_name = (EditText) findViewById(R.id.login_name);
+        login_name.setFocusable(true);
+        login_name.setFocusableInTouchMode(true);
+        login_name.requestFocus();
         login_pass = (EditText) findViewById(R.id.login_pass);
         login = (LinearLayout) findViewById(R.id.login);
         autologin = (CheckBox) findViewById(R.id.autologin);
         login.setOnClickListener(new View.OnClickListener() {
-            String lname = login_name.getText().toString();
-            String pass = login_pass.getText().toString();
+
+
             @Override
             public void onClick(View v) {
+                String lname = login_name.getText().toString();
+                String pass = login_pass.getText().toString();
+                CheckLOGIN cl = new CheckLOGIN(lname, pass);
+                cl.execute();
+                cl.setOnDataFinishedListener(new OnDataFinishedListener() {
+                    @Override
+                    public void onDataSuccessfully(Object data) {
+                        String checked = data.toString();
+                        //Log.i("xml", "truetruetruetruetruetruetruetrue" + checked);
+                        Toast.makeText(LoginActivity.this, "登陆成功！", Toast.LENGTH_SHORT).show();
+                        if (autologin.isChecked()) {
+                            savesp();
+                        }
+                        startActivity(new Intent(LoginActivity.this, MainActivity.class));
+                    }
 
+                    @Override
+                    public void onDataFailed() {
+                        Toast.makeText(LoginActivity.this, "请输入正确的用户名和密码！", Toast.LENGTH_LONG).show();
+                    }
+                });
             }
         });
 
@@ -51,7 +82,25 @@ public class LoginActivity extends Activity {
     }
     private String getsp(){
         SharedPreferences preferences = getSharedPreferences("autologin", Context.MODE_PRIVATE);
-        String flag = preferences.getString("autologin","false");
+        String flag = preferences.getString("autologin", "false");
         return flag;
+    }
+    /**
+     * 判断网络状态
+     * @param context
+     * @return
+     */
+    public static boolean checkNetWorkStatus(Context context){
+        boolean result;
+        ConnectivityManager cm=(ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo netinfo = cm.getActiveNetworkInfo();
+        if ( netinfo !=null && netinfo.isConnected() ) {
+            result=true;
+            Log.i("xml", "The net was connected");
+        }else{
+            result=false;
+            Log.i("xml", "The net was bad!");
+        }
+        return result;
     }
 }
--- app/src/main/java/com/wgz/ant/antinstall/fragment/PersonFragment.java
+++ app/src/main/java/com/wgz/ant/antinstall/fragment/PersonFragment.java
@@ -1,22 +1,55 @@
 package com.wgz.ant.antinstall.fragment;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.wgz.ant.antinstall.LoginActivity;
 import com.wgz.ant.antinstall.R;
+
+import java.io.File;
 
 /**
  * Created by qwerr on 2015/11/16.
  */
 public class PersonFragment extends Fragment {
-    @Nullable
+    private TextView outLogin;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.personfragment,null);
+        initview(view);
         return  view;
+    }
+
+    private void initview(View view) {
+        outLogin = (TextView) view.findViewById(R.id.login_out);
+        outLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+                builder.setTitle("注销").setMessage("是否注销登陆，返回登陆页面？").setPositiveButton("确定", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        File file= new File("/data/data/"+getActivity().getPackageName().toString()+"/shared_prefs","autologin.xml");
+                        if(file.exists()){
+                            file.delete();
+                            Toast.makeText(getActivity().getApplicationContext(), "注销成功！", Toast.LENGTH_LONG).show();
+                            startActivity(new Intent(getActivity(), LoginActivity.class));
+                        }else {
+                            Toast.makeText(getActivity().getApplicationContext(), "注销成功！", Toast.LENGTH_LONG).show();
+                            startActivity(new Intent(getActivity(), LoginActivity.class));
+
+                        }
+                    }
+                }).setNegativeButton("取消",null).show();
+            }
+        });
     }
 }
